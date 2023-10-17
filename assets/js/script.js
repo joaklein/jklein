eb060476318ff76b5f71b618e6f9b2e5a0ca8541
Josh Klein <skiz303@gmail.com>
2023-10-17 15:22:02 -0400

    click fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    projectItems = document.querySelectorAll('.project-item')
+    projectItems = document.querySelectorAll('.project-title')
 
     for (let projectItem of projectItems) {
         projectItem.addEventListener('click', () => {
-            if (projectItem.classList.contains('project-item-open')) {
-                projectItem.classList.remove('project-item-open')
-                projectItem.children[0].children[0].classList.replace('fa-folder-open', 'fa-folder')
+            if (projectItem.parentElement.classList.contains('project-item-open')) {
+                projectItem.parentElement.classList.remove('project-item-open')
+                projectItem.children[0].classList.replace('fa-folder-open', 'fa-folder')
             } else {
-                projectItem.classList.add('project-item-open')
-                projectItem.children[0].children[0].classList.replace('fa-folder', 'fa-folder-open')
+                projectItem.parentElement.classList.add('project-item-open')
+                projectItem.children[0].classList.replace('fa-folder', 'fa-folder-open')
 
             }
         })
